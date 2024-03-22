5c9e630ba554a4e9c4a0ef05055390e4550a30cb
AndrijaLazic <lazic.andrija223@gmail.com>
2024-03-23 00:55:53 +0100

    feat: :sparkles: Implemented navbar

--- FrontendANG/src/app/app.module.ts
+++ FrontendANG/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponent } from './main-page/main-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { NavigationBarComponent } from './shared/components/navigation-bar/navigation-bar.component';
 
 @NgModule({
 	declarations: [AppComponent, MainPageComponent],
-	imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule, NavigationBarComponent],
 	providers: [],
 	bootstrap: [AppComponent],
 })
---
+++ FrontendANG/src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherForecastService } from '../../shared/services/weather-forecast.service';
+
+@Component({
+	selector: 'app-main-page',
+	templateUrl: './main-page.component.html',
+	styleUrls: ['./main-page.component.css'],
+})
+export class MainPageComponent {
+	// ngOnInit(): void {
+	// 	this.service.getData().subscribe((data) => {
+	// 		console.log(data);
+	// 	});
+	// }
+
+	constructor(private service: WeatherForecastService) { }
+}
