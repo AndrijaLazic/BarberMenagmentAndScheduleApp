25c89db1bf949497e7fb6e6b911b8440dc6aa1a2
Jovan Brzakovic <jova3105@gmail.com>
2024-03-23 16:59:34 +0100

    User model + database init

---
+++ BackendAPI/Data/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using BackendAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackendAPI.Data
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User>? Users { get; set; }
+
+        public UserContext(DbContextOptions<UserContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.PhoneNumber)
+                .IsUnique();
+        }
+
+    }
+}
--- BackendAPI/Program.cs
+++ BackendAPI/Program.cs
@@ -1,3 +1,6 @@
+using BackendAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<UserContext>(options =>
+{
+    options.UseSqlServer("Server=(localdb)\\baza;Database=BarberDB;Trusted_Connection=True;TrustServerCertificate=True;");
+});
 
 // Enable CORS
 var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
