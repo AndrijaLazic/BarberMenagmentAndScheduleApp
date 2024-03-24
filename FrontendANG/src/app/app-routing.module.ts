f71efe2301b34f31f6d61fbbded91fdd87bc985a
Jovan Brzakovic <jova3105@gmail.com>
2024-03-24 14:10:10 +0100

    Register page init

--- FrontendANG/src/app/app-routing.module.ts
+++ FrontendANG/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
 	{ path: '', component: MainPageComponent, pathMatch: 'full' },
+	{ path: 'registracija', component: RegisterPageComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
---
+++ FrontendANG/src/app/pages/register-page/register-page.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-register-page',
+	standalone: true,
+	imports: [],
+	templateUrl: './register-page.component.html',
+	styleUrl: './register-page.component.css'
+})
+export class RegisterPageComponent implements OnInit {
+
+	constructor () {}
+
+	ngOnInit (): void {
+		throw new Error('Method not implemented.');
+	}
+
+}
