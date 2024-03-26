93705bec6347a212dc02f3b0c72c0ade8df8cdc0
AndrijaLazic <lazic.andrija223@gmail.com>
2024-03-26 01:18:10 +0100

    feat: Implemented angular enviroment variables

--- FrontendANG/src/app/shared/services/auth.service.ts
+++ FrontendANG/src/app/shared/services/auth.service.ts
@@ -2,17 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RegisterDTS } from '../models/RegisterDTS';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AuthService {
-	
+
+	baseUrl=environment.BACK_END_URL+"Authentication/";
+
 	constructor (private http: HttpClient) {}
 
 	register (body: RegisterDTS):Observable<any> {
 		console.log(body);
-		return this.http.post('https://localhost:7278/Authentication/Register', body);
+		return this.http.post(this.baseUrl+'Register', body);
 	}
-	
+
 }
---
+++ FrontendANG/src/environments/environment.development.ts
@@ -0,0 +1,3 @@
+export const environment = {
+	BACK_END_URL:"https://localhost:7278/"
+};
