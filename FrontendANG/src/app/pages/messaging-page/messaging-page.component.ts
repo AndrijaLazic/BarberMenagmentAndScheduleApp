ce8ea73b7a40102968463c0b7c5066e34f53cb17
Jovan Brzakovic <jova3105@gmail.com>
2024-03-31 00:28:16 +0100

    Messaging init page

---
+++ FrontendANG/src/app/pages/messaging-page/messaging-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-messaging-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './messaging-page.component.html',
+  styleUrl: './messaging-page.component.css'
+})
+export class MessagingPageComponent {
+
+}
